122f79443e496a3190a82d4766405a927b0a313c
yasinviv <159425350+yasinviv@users.noreply.github.com>
2024-02-29 10:23:01 +0100

    Initial commit

---
+++ VivesBlog.Ui.Mvc/Models/Article.cs
@@ -0,0 +1,11 @@
+﻿namespace VivesBlog.Ui.Mvc.Models
+{
+    public class Article
+    {
+        public required string Title { get; set; }
+        public required string Author { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public required string Description { get; set; }
+        public required string Content { get; set; }
+    }
+}
